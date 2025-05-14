38af2b62d72fdd2a32e05933df7987feefc1f9d9
sweety-v <varkolusweety@gmail.com>
2025-05-14 13:04:48 +0530

    Add files via upload

---
+++ Example5.java
@@ -0,0 +1,13 @@
+package com.loop;
+
+public class Example5 {
+	public static void main(String [] args) {
+		for(int i=5; i<=50; i=i+5) {
+			System.out.println(i);
+		}
+	}
+
+}
+
+
+
---
+++ ReverseOrder.java
@@ -0,0 +1,10 @@
+package com.loop;
+
+public class ReverseOrder {
+	public static void main(String [] args) {
+		for(int i=10; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
